34c72678035863eebc5362089afa0013ae031b40
Yuner Bekir <yuner.bekir@gmail.com>
2019-02-16 20:16:44 +0200

    added pois editing and address change

--- client/src/routes/profile/my-pois.component.js
+++ client/src/routes/profile/my-pois.component.js
@@ -3,14 +3,54 @@ import PropTypes from 'prop-types';
 import { Card } from 'antd';
 
 export class MyPoisComponent extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={
+            pois:this.props.pois
+        }
+        this.handleUserInput = this.handleUserInput.bind(this);
+        this.handleButtonClick= this.handleButtonClick.bind(this);
+    }
+    handleUserInput(e,index) {
+        const name = e.target.name;
+        const value = e.target.value;
+
+        this.setState((prevState)=>{
+            return {
+                pois:[...prevState.pois.slice(0,index),{
+                    location: name==='location'?value:prevState.pois[index].location,
+                    hourRange: name==='hourRange'?value:prevState.pois[index].timeRange,
+                },...prevState.pois.slice(index+1,prevState.pois.length)]
+            };
+        })
+
+        console.log(this.state.pois[index]);
+
+    }
+    handleButtonClick(){
+        this.props.onPoisUpdated([...this.state.pois]);
+    }
     render() {
         return (
             <React.Fragment>
                 <Card>
+                    {this.state.pois.map((poi,index)=>{
+                        return <div key={index}>
+                                    <input autoFocus={true}
+                                        type='text' name='location'
+                                        placeholder='Desired location'
+                                        value={poi.location}
+                                        onChange={(e)=>this.handleUserInput(e,index)}/>
+                                    <input autoFocus={true}
+                                        type='text' name='hourRange'
+                                        placeholder='Hour range'
+                                        value={poi.hourRange}
+                                        onChange={(e)=>this.handleUserInput(e,index)}/>
+                        </div>
+                    })}
                     <p>{this.props.pois[0] && this.props.pois[0].location}</p>
-                    <button onClick={() => {
-                        this.props.onPoisUpdated([...this.props.pois, { location: 'qnci', hourRange: '18-19' }]);
-                    }}>Updates
+                    
+                    <button onClick={this.handleButtonClick}>update
                     </button>
                 </Card>
             </React.Fragment>
@@ -22,4 +62,5 @@ MyPoisComponent.propTypes = {
     pois: PropTypes.array.isRequired,
     onPoisUpdated: PropTypes.func.isRequired,
 };
+
 
--- client/src/routes/profile/my-property.component.js
+++ client/src/routes/profile/my-property.component.js
@@ -3,15 +3,34 @@ import PropTypes from 'prop-types';
 import { Card } from 'antd';
 
 export class MyPropertyComponent extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={
+            address:this.props.currentproperty.address
+        }
+        this.handleUserInput = this.handleUserInput.bind(this);
+        this.handleButtonClick= this.handleButtonClick.bind(this);
+    }
+    handleUserInput(e) {
+        const name = e.target.name;
+        const value = e.target.value;
 
+        this.setState({ [name]: value });
+    }
+    handleButtonClick(){
+        this.props.onPropertyUpdate({ ...this.props.currentproperty, address: this.state.address });
+    }
     render() {
         return (
             <React.Fragment>
                 <Card>
                     <p>{this.props.currentproperty.address}</p>
-                    <button onClick={() => {
-                        this.props.onPropertyUpdate({ ...this.props.currentproperty, address: 'ilienci' });
-                    }}>Updates
+                    <input autoFocus={true}
+                               type='text' name='address'
+                               placeholder='Address'
+                               value={this.state.address}
+                               onChange={this.handleUserInput}/>
+                    <button onClick={this.handleButtonClick}>update
                     </button>
                 </Card>
             </React.Fragment>
