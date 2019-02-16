8a33ecc0187683b55c862be369ffc2a8415f7c19
fatih <fatih.hyuseinov@rhythmbg.com>
2019-02-16 12:31:55 +0200

    update user pois and requestedproperties

--- server/api/index.js
+++ server/api/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 const api = express.Router();
 
-api.use('/app-layout', require('./app-layout'));
 api.use('/users', require('./users'));
 
 module.exports = api;
--- server/api/users/users.api.js
+++ server/api/users/users.api.js
@@ -11,15 +11,18 @@ module.exports = {
         `, [id]);
 
         const user = usersSqlResult.rows && usersSqlResult.rows[0];
-        delete user.password;
 
-        res.locals = {
-            data: user,
-            toastMessages: [],
-            confirmMessage: '',
-        };
-
-        next();
+        if (user) {
+            delete user.password;
+            res.locals = {
+                data: user,
+                toastMessages: [],
+                confirmMessage: '',
+            };
+            next();
+        } else {
+            next({ statusCode: 404, message: 'User not found' });
+        }
     },
     getUsers: async (req, res, next) => {
         const usersSqlResult = await db.query(`
@@ -43,6 +46,34 @@ module.exports = {
                                              currentproperty=$2
                                          WHERE id=$3
                                        `, [isactive, currentproperty, id]);
+        res.locals.data = {
+            appLayout: result
+        };
+
+        next();
+    },
+
+    updateUserPois: async (req, res, next) => {
+        const { id } = req.auth.user;
+        const { pois } = req.body;
+        const result = await db.query(`UPDATE ${db.tableNames.users} 
+                                         SET pois=$1
+                                         WHERE id=$2
+                                       `, [pois, id]);
+        res.locals.data = {
+            appLayout: result
+        };
+
+        next();
+    },
+
+    updateUserRequestedProperties: async (req, res, next) => {
+        const { id } = req.auth.user;
+        const { requestedProperties } = req.body;
+        const result = await db.query(`UPDATE ${db.tableNames.users} 
+                                         SET requestedproperties=$1
+                                         WHERE id=$2
+                                       `, [requestedProperties, id]);
         res.locals.data = {
             appLayout: result
         };
