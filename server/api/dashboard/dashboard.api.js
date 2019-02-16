c2808c078588d56e323e8f4468d06af39da7cd13
fatih <fatih.hyuseinov@rhythmbg.com>
2019-02-16 13:21:56 +0200

    dashboard api

--- server/api/dashboard/dashboard.api.js
+++ server/api/dashboard/dashboard.api.js
@@ -5,13 +5,21 @@ const db = require('../../db/index');
 module.exports = {
     getMyDashboard: async (req, res, next) => {
         const { id } = req.auth.user;
-        const usersSqlResult = await db.query(`
+        const activeUsersSqlResult = await db.query(`
             SELECT id, username, email, isactive, currentproperty FROM ${db.tableNames.users}
             WHERE isactive=true
         `);
 
+        const myMatchesSqlResult = await db.query(`
+            SELECT * FROM ${db.tableNames.matches}
+            WHERE $1 < userids
+        `, [`{${id}}`]);
+
         res.locals = {
-            data: usersSqlResult.rows,
+            data: {
+                activeUsers: activeUsersSqlResult.rows,
+                myMatches: myMatchesSqlResult.rows,
+            },
             toastMessages: [],
             confirmMessage: '',
         };
