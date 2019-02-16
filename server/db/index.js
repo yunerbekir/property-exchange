02ad9cf2b8a2f7c56209096ee21859004cd448f6
fatih <fatih.hyuseinov@rhythmbg.com>
2019-02-16 04:11:57 +0200

    initial db structure

--- server/db/index.js
+++ server/db/index.js
@@ -14,6 +14,9 @@ const pool = new Pool({
 const schemaName = 'public';
 const tableNames = {
     users: schemaName + '.users',
+    requestedProperties: schemaName + '.requested_properties',
+    pois: schemaName + '.pois',
+    matches: schemaName + '.matches',
 };
 
 module.exports = {
@@ -28,6 +31,9 @@ module.exports = {
             }
 
             await insertUsersData(tableNames.users);
+            await insertRequestedPropertiesData(tableNames.requestedProperties);
+            await insertPoisData(tableNames.pois);
+            await insertMatchesData(tableNames.matches);
         }
         catch (e) {
             throw e;
@@ -42,15 +48,17 @@ async function insertUsersData(tableName) {
                      username varchar,
                      password varchar,
                      email varchar,
+                     isactive boolean,
                      currentproperty jsonb
                      )`
     );
 
-    await pool.query(`INSERT INTO ${tableName}(id, username, password, email, currentproperty) VALUES($1, $2, $3, $4, $5)`, [
+    await pool.query(`INSERT INTO ${tableName}(id, username, password, email, isactive, currentproperty) VALUES($1, $2, $3, $4, $5, $6)`, [
         '0',
-        'prop',
-        encript(Buffer.from('prop', 'binary').toString('base64')),
-        'prop@gmail.com',
+        'fatih',
+        encript(Buffer.from('fatih', 'binary').toString('base64')),
+        'f.hyuseinov@gmail.com',
+        true,
         {
             'address': 'Sofia bul. Bulgaria 56',
             'rent': 500,
@@ -58,3 +66,56 @@ async function insertUsersData(tableName) {
         }
     ]);
 }
+
+async function insertRequestedPropertiesData(tableName) {
+    await pool.query(
+        `CREATE TABLE IF NOT EXISTS ${tableName}(
+                     id varchar PRIMARY KEY,
+                     user_id varchar,
+                     specs jsonb
+                     )`
+    );
+
+    await pool.query(`INSERT INTO ${tableName}(id, user_id, specs) VALUES($1, $2, $3)`, [
+        '0',
+        '0',
+        {
+            'address': 'Sofia bul. Al. Malinov 11',
+            'rent': 300,
+            'size': 56,
+        }
+    ]);
+}
+
+async function insertPoisData(tableName) {
+    await pool.query(
+        `CREATE TABLE IF NOT EXISTS ${tableName}(
+                     id varchar PRIMARY KEY,
+                     user_id varchar,
+                     location varchar,
+                     hourrange varchar
+                     )`
+    );
+
+    await pool.query(`INSERT INTO ${tableName}(id, user_id, location, hourrange) VALUES($1, $2, $3, $4)`, [
+        '0',
+        '0',
+        'Sofia bul. Al. Malinov 11',
+        '08-09'
+    ]);
+}
+
+async function insertMatchesData(tableName) {
+    await pool.query(
+        `CREATE TABLE IF NOT EXISTS ${tableName}(
+                     id varchar PRIMARY KEY,
+                     userids varchar[]
+                     )`
+    );
+
+    await pool.query(`INSERT INTO ${tableName}(id, userids) VALUES($1, $2)`, [
+        '0',
+        ['0', '1'],
+    ]);
+}
+
