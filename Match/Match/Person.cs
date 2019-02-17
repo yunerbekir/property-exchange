1d6aa1ba4167a1a463c4637d90449f0c6446c37c
Aleksandar Aleksov <a.aleksov1@yahoo.com>
2019-02-17 12:41:16 +0200

    added match algorithm

---
+++ Match/Match/Person.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Match
+{
+    class Person
+    {
+        private string firstName;
+        private string lastName;
+        private string currentNeighborhood;
+        private double currentRent;
+        private double currentSize;
+        private string wantedNeighborhood;
+        private double wantedRent;
+        private double wantedSize;
+
+        public Person()
+        {
+            firstName = "";
+            lastName = "";
+            currentNeighborhood = "";
+            currentRent = 0;
+            currentSize = 0;
+            wantedNeighborhood = "";
+            wantedRent = 0;
+            wantedSize = 0;
+        }
+
+        public Person(string fName, string lName, string cNbh, double cRnt, double cSz, string wNbh, double wRnt, double wSz)
+        {
+            firstName = fName;
+            lastName = lName;
+            currentNeighborhood = cNbh;
+            currentRent = cRnt;
+            currentSize = cSz;
+            wantedNeighborhood = wNbh;
+            wantedRent = wRnt;
+            wantedSize = wSz;
+        }
+
+
+        public string getName()
+        {
+            return firstName + " " + lastName;
+        }
+
+        public string getCurrentNbh()
+        {
+            return currentNeighborhood;
+        }
+
+        public double getCurrentR()
+        {
+            return currentRent;
+        }
+
+        public double getCurrentS()
+        {
+            return currentSize;
+        }
+
+        public string getWantedNbh()
+        {
+            return wantedNeighborhood;
+        }
+
+        public double getWantedR()
+        {
+            return wantedRent;
+        }
+
+        public double getWantedS()
+        {
+            return wantedSize;
+        }
+    }
+}
---
+++ Match/Match/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Match
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Person firstPerson = new Person("Andrej", "Ivanovski", "Skaptopara", 500, 100, "Gramada", 400, 80);
+            Person secondPerson = new Person("Aleksandar", "Aleksov", "Gramada", 700, 110, "Skaptopara", 600, 120);
+
+
+            if (firstPerson.getCurrentNbh() == secondPerson.getWantedNbh() && firstPerson.getWantedNbh() == secondPerson.getCurrentNbh())
+            {
+                Console.WriteLine("Matching neighborhoods!");
+            }
+            else if (firstPerson.getCurrentR() < secondPerson.getWantedR() && firstPerson.getWantedR() > secondPerson.getCurrentR())
+            {
+                Console.WriteLine("Matching rents!");
+            }
+            else if (firstPerson.getCurrentS() > secondPerson.getWantedS() && firstPerson.getWantedS() < secondPerson.getCurrentS())
+            {
+                Console.WriteLine("Matching appartment sizes!");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
